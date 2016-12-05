7cc740e0d6fa1767769863ca6dadbc9887e08c80
firdakhasanah14 <firdakhasanah25@gmail.com>
2016-12-05 22:21:43 +0700

    Add files via upload

---
+++ sensor_suhu.ino
@@ -0,0 +1,22 @@
+const int pSuhu = A1;
+float suhu, data;
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pSuhu, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  data = analogRead(pSuhu);
+  suhu = data / 2.0479;
+  
+  Serial.print("data: ");
+  Serial.print(data);
+  Serial.print(", suhu: ");
+  Serial.print(suhu);
+  Serial.println();
+  delay(1000);
+}
